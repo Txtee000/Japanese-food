6729dfd424a546b70a8ee8fea8323525ea43667f
txtee <satianrutai@gmail.com>
2023-08-15 22:04:28 +0700

    add content and readmore btn

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const parentContainer = document.querySelector(".section2");
+parentContainer.addEventListener("click" , event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes("btn-read");
+    
+    if(!isReadMoreBtn) return;
+
+    const currentText = document.getElementById("read-more");
+
+    currentText.classList.toggle('show'); //สร้าง class show ขึ้นมา
+    current.textContent = current.textContent.includes("ดูเพิ่ม") ? "ย่อ":"ดูเพิ่ม";
+
+})
