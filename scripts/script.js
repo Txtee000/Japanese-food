69b0c8e77666a3e93ee6e81ebe44169cbfdff020
txtee <satianrutai@gmail.com>
2023-08-19 21:32:06 +0700

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 //*******************************Read more btn********************************\\
 const parentContainer = document.querySelector(".section2");
+const addBg = document.querySelector(".container2")
 parentContainer.addEventListener("click" , event=>{
     const current = event.target;
     const isReadMoreBtn = current.className.includes("btn-read");
@@ -7,8 +8,10 @@ parentContainer.addEventListener("click" , event=>{
     if(!isReadMoreBtn) return;
 
     const currentText = document.getElementById("read-more");
+    const currentBg = document.getElementById("container2") //add area container2
 
     currentText.classList.toggle('show'); //สร้าง class show ขึ้นมา
+    currentBg.classList.toggle("add");
     current.textContent = current.textContent.includes("ดูเพิ่ม") ? "ย่อ":"ดูเพิ่ม";
 
 })
